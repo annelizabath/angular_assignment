b9c9454dc5514bd5e7804a488ed15474b1b86e93
annelizabath <33921380+annelizabath@users.noreply.github.com>
2019-02-02 16:07:09 +0530

    Add files via upload

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
-import { ServiceComponent } from './service/service.component';
-import { ApplyComponent } from './apply/apply.component';
 
-const routes: Routes = [{path:'apply',component:ApplyComponent},{path:'login',component:LoginComponent},
-{path:'service',component:ServiceComponent}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,33 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {} from './serice1.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LekshmiComponent } from './lekshmi/lekshmi.component';
-import { HelloworldComponent } from './helloworld/helloworld.component';
-import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
-import { ServiceComponent } from './service/service.component';
-import { ApplyComponent } from './apply/apply.component';
-import { Serice1Service } from './serice1.service';
+import { FooterComponent } from './footer/footer.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LekshmiComponent,
-    HelloworldComponent,
-    ProfileComponent,
     HeaderComponent,
-    LoginComponent,
-    ServiceComponent,
-    ApplyComponent
+    FooterComponent,
+    DashbordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [Serice1Service],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
